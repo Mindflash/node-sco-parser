149e7c6544561febe92df29dd069ef2d178197f5
David Kaminsky <oursfs@gmail.com>
2014-02-07 12:23:59 -0800

    Added more to integration tests.

--- tests/indexTests.js
+++ tests/indexTests.js
@@ -1,24 +1,38 @@
 "use strict";
 var fs = require('fs');
 var test = require('tap').test;
-var scoParser = require('../index.js');
+var scoParserIndex = require('../index.js');
 var testFolder = 'testFiles/unpackScoZipTests';
 var pathToExtractZip = 'testFiles/unpackScoZipTests/extractFolder';
 var rmrf = require('rimraf');
 
-test('Successfully unpacks SCO zip file and validates imsmanifest.xml', function(t) {
+var params = {
+	pathToScoZip: testFolder + '/articulate_sco_with_quiz.zip',
+	pathToExtractZip: pathToExtractZip
+};
+
+test('Successfully unpacks SCO zip file, validates imsmanifest.xml, and parses it for information', function(t) {
+	var params = {
+		pathToScoZip: testFolder + '/articulate_sco_with_quiz.zip',
+		pathToExtractZip: pathToExtractZip
+	};
+	var scoParser = scoParserIndex(params);
 	t.test('Should extract SCO zip file and validate imsmanifest.xml', function (t) {
-		var params = {
-			pathToScoZip: testFolder + '/articulate_sco_with_quiz.zip',
-			pathToExtractZip: pathToExtractZip
-		};
-		scoParser(params).validate(function (err, result) {
+		scoParser.validate(function (err, result) {
 			t.notOk(err, 'Should not error');
 			t.ok(fs.existsSync(pathToExtractZip), 'Should have created path in which to extract zip');
 			t.ok(result, 'Should receive XML JSONified');
-			t.equal(result.manifest.resources[0].resource[0].$.href, 'index_lms.html', 'Should find the main html file');
 			t.end();
 		});
+	});
+
+	t.test('Should parse imsmanifest.xml to find base html file', function(t) {
+		scoParser.parse(function (err, result) {
+			t.notOk(err, 'Should not error');
+			t.ok(result, 'Should receive information parsed from the manifest XML');
+			t.equal(result.scoHtmlHref, 'index_lms.html', 'Should receive the SCO\'s start-up HTML file from the manifest');
+			t.end();
+		})
 	});
 
 	t.test('Deletes folder in which to unzip the files', function (t) {

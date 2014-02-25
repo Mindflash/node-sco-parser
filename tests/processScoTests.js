7ee101a26b246e439c8c87ab6faa6540bb3202fb
David Kaminsky <oursfs@gmail.com>
2014-02-24 16:55:23 -0800

    Process Captivate so that its output uses wmode opaque for Flash.

--- tests/processScoTests.js
+++ tests/processScoTests.js
@@ -23,7 +23,7 @@ test('Errors when you don\'t pass the path of the manifest', function (t) {
 });
 
 test('Does not error when referencing a SCO that doesn\'t have the Storyline mobile/player_compiled.js', function (t) {
-	t.test('Should copy files for processing', _.curry(copyFilesToProcessingLocation)('testFiles/processScoTests/notStorylineHtml5Output'));
+	t.test('Should copy files for processing', _.curry(copyFilesToProcessingLocation)('testFiles/processScoTests/nonSpecificOutput'));
 
 	t.test('Does not error when referencing a SCO that doesn\'t have the Storyline mobile/player_compiled.js', function (t) {
 		processSco({pathOfManifest: pathToCopyFiles + '/imsmanifest.xml'}, function (err, result) {
@@ -38,12 +38,40 @@ test('Does not error when referencing a SCO that doesn\'t have the Storyline mob
 test('Successfully modifies a SCO that has the Storyline mobile/player_compiled.js', function (t) {
 	t.test('Should copy files for processing', _.curry(copyFilesToProcessingLocation)('testFiles/processScoTests/storylineHtml5Output'));
 
-	t.test('Does not error when referencing a SCO that doesn\'t have the Storyline mobile/player_compiled.js', function (t) {
+	t.test('Should find modfied text in the Storyline mobile/player_compiled.js', function (t) {
 		processSco({pathOfManifest: pathToCopyFiles + '/imsmanifest.xml'}, function (err, result) {
 			t.notOk(err, 'Should not error');
 			var playerCompiledJsString = fs.readFileSync(pathToCopyFiles + '/mobile/player_compiled.js', {encoding: 'utf8'});
 			t.equal(playerCompiledJsString.indexOf('var selfTop=self;'), 0, 'Should find JS added at beginning of the file');
 			t.ok(playerCompiledJsString.indexOf('selfTop.', 20) > 0, 'Should find other instances of selfTop from the replacement later in the file');
+			t.end();
+		});
+	});
+
+	t.test('Should remove temporary files copied for processing', removeTempProcessingLocation);
+});
+
+test('Does not error when referencing a SCO that doesn\'t have the Captivate SCORM_utilities.js', function (t) {
+	t.test('Should copy files for processing', _.curry(copyFilesToProcessingLocation)('testFiles/processScoTests/nonSpecificOutput'));
+
+	t.test('Does not error when referencing a SCO that doesn\'t have the Captivate SCORM_utilities.js', function (t) {
+		processSco({pathOfManifest: pathToCopyFiles + '/imsmanifest.xml'}, function (err, result) {
+			t.notOk(err, 'Should not error');
+			t.end();
+		});
+	});
+
+	t.test('Should remove temporary files copied for processing', removeTempProcessingLocation);
+});
+
+test('Successfully modifies a SCO that has the Captivate SCORM_utilities.js', function (t) {
+	t.test('Should copy files for processing', _.curry(copyFilesToProcessingLocation)('testFiles/processScoTests/captivateOutput'));
+
+	t.test('Should find modfied text in the Captivate SCORM_utilities.js', function (t) {
+		processSco({pathOfManifest: pathToCopyFiles + '/imsmanifest.xml'}, function (err, result) {
+			t.notOk(err, 'Should not error');
+			var playerCompiledJsString = fs.readFileSync(pathToCopyFiles + '/SCORM_utilities.js', {encoding: 'utf8'});
+			t.ok(playerCompiledJsString.indexOf('WMODE : "opaque"') >= 0, 'Should find wmode config now set to opaque');
 			t.end();
 		});
 	});

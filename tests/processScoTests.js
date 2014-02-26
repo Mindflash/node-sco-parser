8fff711fca36e82ca02258485e63b340f8d63d18
David Kaminsky <oursfs@gmail.com>
2014-02-25 16:50:00 -0800

    More processing of Storyline mobile JS to work better in landscape mode.

--- tests/processScoTests.js
+++ tests/processScoTests.js
@@ -38,12 +38,22 @@ test('Does not error when referencing a SCO that doesn\'t have the Storyline mob
 test('Successfully modifies a SCO that has the Storyline mobile/player_compiled.js', function (t) {
 	t.test('Should copy files for processing', _.curry(copyFilesToProcessingLocation)('testFiles/processScoTests/storylineHtml5Output'));
 
+	t.test('Should have code that\'s prior to processing', function (t) {
+		var playerCompiledJsString = fs.readFileSync(pathToCopyFiles + '/mobile/player_compiled.js', {encoding: 'utf8'});
+		t.ok(playerCompiledJsString.indexOf('var k=j-window.innerHeight') >= 0, 'Should still have this code');
+		t.ok(playerCompiledJsString.indexOf('if(self!=top)a=a.replace("framewrap","portrait .framewrap").replace("interstitial","portrait .interstitial"),player.rotatePortraitFramedStyleSheet=document.createElement("style"),player.rotatePortraitFramedStyleSheet.innerHTML=a,document.body.appendChild(player.rotatePortraitFramedStyleSheet)') >= 0, 'Should still have this code');
+		t.end();
+	});
+
 	t.test('Should find modfied text in the Storyline mobile/player_compiled.js', function (t) {
 		processSco({pathOfManifest: pathToCopyFiles + '/imsmanifest.xml'}, function (err, result) {
 			t.notOk(err, 'Should not error');
 			var playerCompiledJsString = fs.readFileSync(pathToCopyFiles + '/mobile/player_compiled.js', {encoding: 'utf8'});
 			t.equal(playerCompiledJsString.indexOf('var selfTop=self;'), 0, 'Should find JS added at beginning of the file');
 			t.ok(playerCompiledJsString.indexOf('selfTop.', 20) > 0, 'Should find other instances of selfTop from the replacement later in the file');
+			t.ok(playerCompiledJsString.indexOf('var k=j-window.innerHeight') == -1, 'Should have removed this code');
+			t.ok(playerCompiledJsString.indexOf('var k=0') >= 0, 'The code above should have been replaced with this code.');
+			t.ok(playerCompiledJsString.indexOf('if(self!=top)a=a.replace("framewrap","portrait .framewrap").replace("interstitial","portrait .interstitial"),player.rotatePortraitFramedStyleSheet=document.createElement("style"),player.rotatePortraitFramedStyleSheet.innerHTML=a,document.body.appendChild(player.rotatePortraitFramedStyleSheet)') == -1, 'Should have removed this code');
 			t.end();
 		});
 	});

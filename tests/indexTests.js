1007b8594b37a7fdb582530d4dca82c76bac791b
jiverson <jiverson222@gmail.com>
2018-03-15 14:11:34 -0700

    feat: add in articulate rise scorm type

--- tests/indexTests.js
+++ tests/indexTests.js
@@ -3,13 +3,14 @@ const fs = require('fs');
 const scoParserIndex = require('../index.js');
 const test = require('tap').test;
 
-const params = {
-  pathToScoZip: 'tests/testFiles/unpackScoZipTests/articulate_sco_with_quiz.zip',
-  pathToExtractZip: 'tests/testFiles/unpackScoZipTests/extractFolder'
-};
 
 test('Successfully unpacks SCO zip file, validates imsmanifest.xml, and parses it for information', (t) => {
+  const params = {
+    pathToScoZip: 'tests/testFiles/unpackScoZipTests/articulate_sco_with_quiz.zip',
+    pathToExtractZip: 'tests/testFiles/unpackScoZipTests/extractFolder'
+  };
   const scoParser = scoParserIndex(params);
+
   t.test('Should extract SCO zip file and validate imsmanifest.xml', (t) => {
     scoParser.validate((err, result) => {
       t.notOk(err, 'Should not error');
@@ -33,6 +34,41 @@ test('Successfully unpacks SCO zip file, validates imsmanifest.xml, and parses i
   t.test('Should process SCO', (t) => {
     scoParser.process((err) => {
       t.notOk(err, 'Should not error');
+      t.end();
+    });
+  });
+
+  t.test('Deletes folder in which to unzip the files', (t) => {
+    scoParser.destroy((err) => {
+      t.notOk(err, 'failed to remove directory where files were unzipped to');
+      t.end();
+    });
+  });
+
+  t.end();
+});
+
+test('Successfully unpacks articulate rise SCO zip file, validates imsmanifest.xml, and parses it for information', (t) => {
+  const params = {
+    pathToScoZip: 'tests/testFiles/unpackScoZipTests/articulate_rise_with_quiz.zip',
+    pathToExtractZip: 'tests/testFiles/unpackScoZipTests/extractFolder'
+  };
+  const scoParser = scoParserIndex(params);
+
+  t.test('Should extract SCO zip file and validate imsmanifest.xml', (t) => {
+    scoParser.validate((err, result) => {
+      t.notOk(err, 'Should not error');
+      t.ok(fs.existsSync(params.pathToExtractZip), 'Should have created path in which to extract zip');
+      t.ok(result, 'Should receive result');
+      t.ok(result.manifest, 'Should receive manifest data');
+      t.end();
+    });
+  });
+
+  t.test('Should process SCO', (t) => {
+    scoParser.process((err, result) => {
+      t.notOk(err, 'Should not error');
+      t.equal(result, 'articulate_rise');
       t.end();
     });
   });
